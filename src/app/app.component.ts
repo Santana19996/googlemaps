8fd919bf1014b12aed498833cc09c420889369af
Oscar Santana <78528593+Santana19996@users.noreply.github.com>
2022-09-27 10:31:52 -0500

    adding code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Loader} from "@googlemaps/js-api-loader";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'GoogleMapsAngular';
+export class AppComponent implements OnInit{
+  title = 'google-maps';
+
+  ngOnInit(): void {
+    let loader = new Loader({
+      apiKey: 'AIzaSyC1prN1bEET8WlNMVP-_r05U9ABMG2mPSQ',
+    })
+
+    loader.load().then(() => {
+      new google.maps.Map(<HTMLElement>document.getElementById("map"), {
+        center: {lat: 31.136906, lng: -97.433624},
+        zoom: 15,
+
+      })
+    })
+  }
 }
